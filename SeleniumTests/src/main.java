158c1323c5d727a2411db2bb9961bcd2a3e62c7f
Winton <mwinton@dxc.com>
2020-11-13 09:33:01 +1300

    Configuration working

---
+++ SeleniumTests/src/main.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.TimeUnit;
+ 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "src/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	}
+
+}
